f5f93f6d8872d4ebf153c201da4913fe4bf741c6
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2016-12-30 14:58:06 -0200

    Randomically searching pictures

--- flickrsearch/src/picture/RandomPicture.js
+++ flickrsearch/src/picture/RandomPicture.js
@@ -3,27 +3,35 @@ import axios from '../../node_modules/axios/lib/axios.js';
 import './Picture.css';
 
 class Picture extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
-      pictures: []
+      pictures: [],
+      term: ''
     }
   }
 
+  random() {
+    var tags = ['children', 'flower', 'candies', 'dogs', 'cats', 'car', 'butterfly']
+    var rand = Math.floor((Math.random() * 7))
+
+    this.setState({term: tags[rand]});
+    return tags[rand]
+  }
+
   componentDidMount() {
-    var _this = this;
+    var _this = this
+    var tag = this.random()
+
     var url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&"
-        url += "api_key=f2d3e882afae6220e175c126c6b971ed&tags=children&per_page=30&"
-        url += "tag_mode=all&format=json&nojsoncallback=?"
+        url += "api_key=f2d3e882afae6220e175c126c6b971ed&tags="+ tag + "&"
+        url += "per_page=30&tag_mode=all&format=json&nojsoncallback=?"
     this.JSONRequest =
       axios
         .get(url)
         .then(function(result) {
-          console.log(result);
-          _this.setState({
-            pictures: result.data.photos.photo
-          });
+          _this.setState({pictures: result.data.photos.photo});
         })
   }
 
@@ -34,10 +42,11 @@ class Picture extends Component {
   render() {
     return (
       <div className="container picture-content">
+        <h3 className="title"><strong>Keyword:</strong> <span className="term">{this.state.term}</span></h3>
         <div className="photos">
         {this.state.pictures.map(function(photo) {
           return (
-            <img src={"http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/"
+            <img key={photo.id} src={"http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/"
               + photo.id + "_" + photo.secret + "_q.jpg"} alt={photo.title}/>
           );
         })}
--- flickrsearch/src/random/Random.js
+++ flickrsearch/src/random/Random.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import axios from '../../node_modules/axios/lib/axios.js';
 import './Random.css';
 
 import Header from '../header/Header';
-import Picture from '../picture/Picture';
+import RandomPicture from '../picture/RandomPicture';
 import Footer from '../footer/Footer';
 
 class Random extends Component {
@@ -11,8 +10,7 @@ class Random extends Component {
     return (
       <div className="container main-content">
         <Header />
-        <h3 className="title"><strong>Keyword:</strong> Children</h3>
-        <Picture />
+        <RandomPicture />
         <Footer />
       </div>
     );
